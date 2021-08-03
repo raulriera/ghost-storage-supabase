40b3d280cf5a9554a1267f36ed21d15844b6dee1
Raul Riera <rieraraul@gmail.com>
2021-08-03 17:59:28 -0400

    Initial release

---
+++ README.md
@@ -0,0 +1,35 @@
+# ghost-storage-supabase
+
+[Supabase](https://supabase.io) storage adapter for [Ghost](https://ghost.org). Recommended as a completely free storage solution for blogs being hosted on platforms with ephemeral filesystems, like [Heroku](https://heroku.com).
+
+## Installation
+
+```bash
+cd /path/to/your/ghost/installation
+mkdir -p content/adapters/storage/supabase
+```
+
+After that, simply copy the contexts of this repo into that newly created directory.
+
+## Usage
+
+Add the following to your configuration file and modify it accordingly.
+
+```json
+"storage": {
+    "active": "ghost-storage-supabase",
+    "ghost-storage-supabase": {
+        "bucket": "<your public bucket>",
+        "supabaseUrl": "<your supabase url>",
+        "supabaseKey": "<your supabase anon key>"
+    }
+}
+```     
+
+## Credit
+
+Inspiration of this README came from https://github.com/ifvictr/ghost-storage-github
+
+## License
+
+[MIT License](LICENSE.txt)
---
+++ index.js
@@ -0,0 +1,91 @@
+const BaseAdapter = require("ghost-storage-base");
+const { createClient } = require('@supabase/supabase-js');
+const fs = require('fs').promises;
+const fetch = require('node-fetch');
+const debug = require('ghost-ignition').debug('adapter');
+
+class SupabaseStorageAdapter extends BaseAdapter {
+    constructor(options = {}) {
+        super();
+
+        this.bucketName = options.bucket || 'ghost-bucket'
+        this.client = createClient(options.supabaseUrl, options.supabaseKey);
+    }
+
+    async save(file, targetDir) {
+        const dir = targetDir || this.getTargetDir()
+        const fileName = await this.getUniqueFileName(file, dir)
+        const data = await fs.readFile(file.path)
+
+        debug(`save:`, `${JSON.stringify(file)} | ${fileName}`)
+
+        return Promise.all([
+            this.client
+                .storage
+                .from(this.bucketName)
+                .upload(fileName, data, {
+                    cacheControl: 3600,
+                    upsert: false,
+                    contentType: file.contentType
+                })
+        ])
+            .then(([_, error]) => {
+                if (error) {
+                    throw error
+                }
+
+                const { data } = this.client
+                    .storage
+                    .from(this.bucketName)
+                    .getPublicUrl(fileName)
+
+                return data.publicURL
+            })
+    }
+
+    exists(filename, targetDir) {
+        const dir = targetDir || this.getTargetDir()
+        const filepath = `${dir}/${filename}`
+
+        debug(`exists:`, `${filepath}`)
+
+        const { data, error } = this.client
+            .storage
+            .from(this.bucketName)
+            .getPublicUrl(filepath)
+
+        return fetch(data.publicURL).then(res => {
+            return res.status === 200
+        });
+    }
+
+    serve() {
+        // No need to serve because absolute URLs are returned
+        return (req, res, next) => {
+            next();
+        }
+    }
+
+    delete(fileName, targetDir) {
+        const dir = targetDir || this.getTargetDir()
+        const filepath = `${dir}/${fileName}`
+
+        debug(`delete:`, `${filepath}`)
+
+        return this.client
+            .storage
+            .from(this.bucketName)
+            .remove([filepath])
+    }
+
+    read(options) {
+        debug(`read:`, `${JSON.stringify(options)}`)
+
+        return this.client
+            .storage
+            .from(this.bucketName)
+            .download(options.path)
+    }
+}
+
+module.exports = SupabaseStorageAdapter;
